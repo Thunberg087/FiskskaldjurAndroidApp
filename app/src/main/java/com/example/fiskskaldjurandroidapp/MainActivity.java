2bf4cffd7d2a979960558f3880c51901f5943d97
PutteAbrahamsson <Puttemaden98@hotmail.com>
2020-01-25 12:05:51 +0100

    Created an login system. Fetching data from MySQL to check if login is valid.

--- app/src/main/java/com/example/fiskskaldjurandroidapp/MainActivity.java
+++ app/src/main/java/com/example/fiskskaldjurandroidapp/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.fiskskaldjurandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -13,23 +20,39 @@ import java.sql.*;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        MySQLFunctions.login("thun", "pass");
-
-
     }
 
+    public void loginUser(View view) {
+        //Retrieving data by ID.
+        EditText username = findViewById(R.id.login_username);
+        EditText password = findViewById(R.id.login_password);
 
+        //Converting EditText to Strings.
+        String user = username.getText().toString();
+        String pass = password.getText().toString();
 
+        //Sending a login request to MySQL.
+        boolean x = MySQLFunctions.login(user, pass);
 
+        if(x == true){
+            Toast.makeText(getApplicationContext(), "Inloggning lyckades.", Toast.LENGTH_LONG).show();
 
+            Intent intent = new Intent(this, StartScreenFragmentHolder.class);
+            startActivity(intent);
 
+            //Removing login page from backstack to prevent navigating back.
+            finish();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Fel användarnamn eller lösenord.", Toast.LENGTH_LONG).show();
+        }
 
+    }
 
 }
 
--- app/src/main/java/com/example/fiskskaldjurandroidapp/MySQLFunctions.java
+++ app/src/main/java/com/example/fiskskaldjurandroidapp/MySQLFunctions.java
@@ -1,47 +1,60 @@
 package com.example.fiskskaldjurandroidapp;
 
-import com.jcabi.aspects.Parallel;
+import android.net.IpSecManager;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+
 public class MySQLFunctions {
 
-
-
-    @Parallel
+    private static boolean isValid = false;
     public static boolean login(final String username, final String password) {
 
-        boolean isValid = false;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Class.forName("com.mysql.jdbc.Driver");
+                    Connection con= DriverManager.getConnection(
+                            "jdbc:mysql://78.71.86.80:3306/testAppFiskskaldjur","fiskskaldjur","regergerg");
+                    Statement stmt=con.createStatement();
+                    String query = "SELECT passHash FROM test WHERE username = '"+ username +"'";
+                    ResultSet rs=stmt.executeQuery(query);
 
+                    while(rs.next()) {
+                        isValid = BCrypt.checkpw(password, rs.getString(1));
 
-        try{
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con= DriverManager.getConnection(
-                    "jdbc:mysql://78.71.86.80:3306/testAppFiskskaldjur","fiskskaldjur","regergerg");
-            Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery("SELECT passHash FROM test WHERE username = " + username);
-
-            while(rs.next()) {
-
-                boolean s = BCrypt.checkpw("password", rs.getString(1));
-                isValid = s;
+                        System.out.println(isValid + "<.daspdfsafpa");
+                    }
+                    con.close();
+                    Thread.currentThread().interrupt();
+                }
+                catch (Exception e){
+                    System.out.println(e.getMessage());
+                }
             }
+        }).start();
 
-
-            con.close();
+        //Waiting for thread to finish.
+        try{
+            Thread.currentThread().join(1000); //Får se hur stabil denna blir. :)
         }
-        catch(Exception e)
-        {
-            System.out.println(e);
+        catch(InterruptedException e){
+            System.out.println(e.getMessage());
         }
 
+
+        System.out.println(isValid + " isvalid5");
         return isValid;
     }
 
+
 }
+
+
 
 
 
